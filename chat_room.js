c86eeeef284665cb4d047aad897817269c703ac3
Ud17narayanpal <143936962+Ud17narayanpal@users.noreply.github.com>
2024-03-11 18:33:36 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,25 @@
+function getData()  {firebase.database().ref("/").on('value',
+ function(snapshot) {document.getElementById("output").innerHTML =
+  "";snapshot.forEach(function (childSnapshot) {childKey  =  
+    childSnapshot.key ;
+    Room_names = childKey;
+    //Start code
+    
+    //End code
+ });});}
+
+ getData();
+ function addRoom()
+{
+      room_name = document.getElementById("room_name").ariaValueMax;
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+}
+
+ 
